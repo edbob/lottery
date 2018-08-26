917f27d0e95f75238f3290d67bbd8683dede25a1
edbob <nedov@outlook.com>
2018-08-27 00:34:45 +0300

    add Handlebars

--- server.js
+++ server.js
@@ -2,31 +2,39 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     redis = require('redis'),
-    path = require('path');
+    path = require('path'),
+    hbs = require('express-handlebars');
 
 let redisClient = redis.createClient();
 redisClient.on('connect', () => { console.log('Connected to Redis...') });
 redisClient.on('error', (err) => { console.log("Error" + err) });
 
 const app = express();
-//const router = express.Router();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.get('/', (req, res, nex) => {
-    res.sendFile('index.html');
+app.engine('handlebars', hbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride('_method'));
+
+app.get('/', function (req, res, nex) {
+    res.render('home');
 });
 
 app.get('/rules', (req, res, nex) => {
-    res.sendFile(path.resolve('./public/rules.html'));
+    res.render('rules');
 });
 
 app.get('/team', (req, res, nex) => {
-    res.sendFile(path.resolve('./public/team.html'));
+    res.render('team');
 });
 
 app.get('/contact', (req, res, nex) => {
-    res.sendFile(path.resolve('./public/contact.html'));
+    res.render('contact');
 });
 
 app.listen(3000, () => {
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'wishmaster.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'views')
   },
 
   watchOptions: {
